31e7773f2e211002de4e0409a233c635160d8a94
yunbinam <67604898+yunbinam@users.noreply.github.com>
2020-10-01 23:15:59 -0700

    Upload function mysamples (sample generator)

---
+++ regression test file/Simulation/mysamples.m
@@ -0,0 +1,38 @@
+function [X, beta] = mysamples(R1, n, m, v, noise) 
+%
+% Function: mysamples(R1,n,m)
+% simulation sample generator
+% outputs X: sample matrix, beta: smooth coefficients
+% 
+% Required arguments
+%%% R1: stiff matrix
+%%% n: the number of eigen functions
+%%% m: the number of samples
+%%% v: variance of random coefficients
+%
+% Optional arguments
+% noise: random noise on each vertice
+%
+
+if (nargin == 4)
+    noise = 0;
+end
+
+% generate smooth functions on the manifold
+[V, D] = eigs(R1, n, 'smallestabs');
+
+beta = V(:,round(n/2));
+X = zeros(1, length(R1));
+
+for i = 1:m
+    r = randi([1 n],[1, 10]);
+    e = V(:,r);
+    e = e';
+    c = normrnd(0,v,[1, 10]);
+    x = c*e + normrnd(0,noise,[1,length(R1)]);
+    
+    X(i,:)=x;
+end
+
+end
+
