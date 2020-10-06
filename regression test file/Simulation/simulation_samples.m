527d59e891c36c7174150aac0ffbebe9b4348e0a
yunbinam <67604898+yunbinam@users.noreply.github.com>
2020-10-05 22:14:23 -0700

    update the version with random noise in y

--- regression test file/Simulation/simulation_samples.m
+++ regression test file/Simulation/simulation_samples.m
@@ -1,9 +1,11 @@
 clear
-addpath('brain/regression test file/Simulation')
+addpath('brain/regression test file')
 R1 = mmread('R1.mtx');
 
 [X, beta] = mysamples(R1, 100, 50, 9, .01);
-
+y = X*beta + normrnd(0, 1, [size(X,1),1]);
+A = [y X];
 
 save('beta.mat', 'beta');
 save('X_samples.mat', 'X');
+save('A_samples.mat', 'A');
