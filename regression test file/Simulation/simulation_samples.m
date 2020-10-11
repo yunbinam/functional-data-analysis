e90b58f009dc05741fdd742aea1ce29a25cfbf6e
yunbinam <67604898+yunbinam@users.noreply.github.com>
2020-10-10 23:31:30 -0700

    add eigenfunctions as output

--- regression test file/Simulation/simulation_samples.m
+++ regression test file/Simulation/simulation_samples.m
@@ -2,10 +2,11 @@ clear
 addpath('..')
 R1 = mmread('R1_642.mtx');
 
-[X, beta] = mysamples(R1, 100, 50, 9, .01);
+[X, beta, eigV] = mysamples(R1, 100, 50, 9, .01);
 y = X*beta + normrnd(0, 1, [size(X,1),1]);
 A = [y X];
 
+save('eigV.mat', 'eigV');
 save('beta_642.mat', 'beta');
 save('X_samples_642.mat', 'X');
 save('A_samples_642.mat', 'A');
