06ba03128ba20b2ec5196ea88f3f6346e16e165b
yunbinam <67604898+yunbinam@users.noreply.github.com>
2020-10-12 21:36:33 -0700

    Compare the simulation results

--- regression test file/Simulation/simulation result.R
+++ regression test file/Simulation/simulation result.R
@@ -2,12 +2,13 @@
 # sample:       generated samples, matrix A: [y X]
 # R0:           mass matrix    
 # R1:           stiffness matrix
+# method:       one of comparing methods: cv_spareg, smooth_recon_x, cv_smooth_ols_x
 # p:            proportion of training samples
 # K:            the number of cross validation folds
 # seq.l:        sequence of tuning parameter lambda
 # random.folds: logical
 
-oneSim <- function(sample, R0, R1,
+oneSim <- function(sample, R0, R1, method = "cv_spareg",
                    p = 0.5, K = 5, seq.l = 10^(seq(-5,5,1))){
         y <- sample$y
         X <- sample$X
@@ -22,10 +23,21 @@ oneSim <- function(sample, R0, R1,
         y_train <- y[train]
         y_test <- y[test]
         
-        cv.fit <- cv_spareg(y_train,X_train,R0,R1,K,seq.l)
-        l.opt <- cv.fit$min_lambda
+        if(method=="cv_spareg"){
+                spareg.fit <- cv_spareg(y_train,X_train,R0,R1,K,seq.l)
+                coef <- spareg$coef
+        }
         
-        coef <- spareg(y_train, X_train, R0, R1, l.opt)
+        if(method=="smooth_recon_x"){
+                recon.fit <- smooth_recon_X(y_train,X_train,R0,R1,seq.l)
+                coef <- recon.fit$coef
+        }
+        
+        if(method=="cv_smooth_ols_x"){
+                ols.fit <- cv_smooth_ols_x(y_train,X_train,R0,R1,K,seq.l)
+                coef <- ols.fit$coef
+        }
+        
         predict <- X_test%*%coef$beta+coef$intercept
         mse <- mean((y_test-predict)^2)
         
--- regression test file/Simulation/test_simulation.R
+++ regression test file/Simulation/test_simulation.R
@@ -2,6 +2,7 @@ library(Matrix)
 library(glmnet)
 library(R.matlab)
 library(ggplot2)
+library(dplyr)
 #path='\\\\fs2-vip/students/wenboz4/Desktop/high dimension project/DataAD/control group data/spareg'
 #path='/Users/yunbi/Library/Mobile Documents/com~apple~CloudDocs/20_6_summer/Eardi/brain/regression test file'
 #setwd(path)
@@ -18,13 +19,30 @@ eigV <- readMat('Simulation/eigV.mat')$eigV
 R0 <- readMM('R0_642.mtx')
 R1 <- readMM('R1_642.mtx')
 
-sample <- mysamples(eigV, 15, 500, 4, 1, 9)
+sample_noise <- mysamples(eigV, 10, 500, 4, 4, 1)
+sample_smooth <- mysamples(eigV, 10, 500, 4, 0, 1)
 
 #-----------------------------------------
 #smoothing coefficients
 
-mse <- replicate(100, oneSim(sample, R0, R1))
-boxplot(mse)
+mse1 <- replicate(100, oneSim(sample_noise, R0, R1, method="cv_spareg"))
+mse2 <- replicate(100, oneSim(sample_noise, R0, R1, method="smooth_recon_x"))
+mse3 <- replicate(100, oneSim(sample_noise, R0, R1, method="cv_smooth_ols_x"))
+
+mse1_s <- replicate(100, oneSim(sample_smooth, R0, R1, method="cv_spareg"))
+mse2_s <- replicate(100, oneSim(sample_smooth, R0, R1, method="smooth_recon_x"))
+mse3_s <- replicate(100, oneSim(sample_smooth, R0, R1, method="cv_smooth_ols_x"))
+
+rst <- data.frame("method"=c(rep("Smoothing beta", length(mse1)), 
+                             rep("Smoothing x with recon", length(mse2)), 
+                             rep("Smoothing x with cv", length(mse3))),
+                  "sample"=c(rep("Noisy", length(mse1)+length(mse2)+length(mse3)),
+                             rep("Smooth", length(mse1_s)+length(mse2_s)+length(mse3_s))),
+                  "MSE"=c(mse1, mse2, mse3, mse1_s, mse2_s, mse3_s))
+
+rst %>% group_by(sample) %>%
+        ggplot(rst, aes(x=method, y=MSE, fill=method)) +
+        geom_boxplot()
 
 
 # compare beta* and beta
