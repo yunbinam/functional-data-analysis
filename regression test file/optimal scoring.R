d7761f118a51a05fa1aaf4055ed52441993300de
yunbinam <67604898+yunbinam@users.noreply.github.com>
2020-11-10 17:20:32 -0800

    Update optimal scoring function

--- regression test file/optimal scoring.R
+++ regression test file/optimal scoring.R
@@ -1,6 +1,5 @@
 # Arguments:
-# Y:          n by K indicator matrix of classes (response vector)
-# X:          n by s design matrix (covariates)
+m# X:          n by s design matrix (covariates)
 # R0:         s by s symmetric mass matrix
 # R1:         s by s symmetric stiffness matrix
 # lambda:     tuning parameter of smoothness penalty
@@ -9,19 +8,19 @@
 # 
 
 ## simple example
-# Y <- matrix(c(0,1,0,
-#               1,0,0,
-#               1,0,0,
-#               0,0,1,
-#               0,0,1,
-#               1,0,0,
-#               0,1,0), ncol=3, byrow=TRUE)
-# 
-# X <- matrix(runif(7*642), ncol=642)
-# 
-# R0 <- readMM('../R0_642.mtx')
-# R1 <- readMM('../R1_642.mtx')
-# lambda <- 1
+Y <- matrix(c(0,1,0,
+              1,0,0,
+              1,0,0,
+              0,0,1,
+              0,0,1,
+              1,0,0,
+              0,1,0), ncol=3, byrow=TRUE)
+
+X <- matrix(runif(7*642), ncol=642)
+
+R0 <- Matrix::readMM('R0_642.mtx')
+R1 <- Matrix::readMM('R1_642.mtx')
+lambda <- 1
 
 opt.score <- function(Y, X, R0, R1, lambda){
         
@@ -37,9 +36,9 @@ opt.score <- function(Y, X, R0, R1, lambda){
         
         R0til.inv <- Matrix::Diagonal(x=1/Matrix::rowSums(R0))
         L <- R1 %*% R0til.inv %*% R1
-        S <- chol(L)
+        S <- Matrix::chol(L)
         centered.X <- X - mean(X)
-        X.star <- rbind(centered.X, sqrt(lambda)*t(S))
+        X.star <- rbind(centered.X, sqrt(lambda)*t(as.matrix(S)))
         
         ## 3. pre-specify theta1
         Q <- as.matrix(rep(1, ncol(Y)))
@@ -66,13 +65,31 @@ opt.score <- function(Y, X, R0, R1, lambda){
                 
                 ## c. add new theta
                 new.theta <- (diag(K) - as.matrix(Q[,i])%*%t(as.matrix(Q[,i]))%*%D) %*% solve(D)%*%t(Y)%*%X%*%beta
-                new.norm <- as.numeric(t(new.theta)%*%D%*%new.theta)
+                new.norm <- as.numeric(t(as.matrix(new.theta))%*%D%*%as.matrix(new.theta))
                 new.norm.theta <- new.theta * sqrt(1/new.norm)
                 Q <- cbind(Q, new.norm.theta)
                 ## are the set of scores mutually orthogonal?
                 ## pracma::dot(Q[,2],Q[,3])
         }
         
-        ## LDA
+        ## LDA with the first (one) discriminant direction
+        mu_k <- matrix(0, nrow = s, ncol = K) # centroid vectors
+        pi_k <- vector(length = K)
+        sigma <- matrix(0, nrow = s, ncol = s) # covariance matrix
+        
+        for (i in 1:K){
+                tmp <- X[Y[,i]==1,]
+                pi_k[i] <- nrow(tmp)/n
+                mu_k[,i] <- colMeans(tmp)
+                for(j in 1:nrow(tmp)){
+                        sigma <- sigma + (tmp[j,] - mu_k[,i]) %*% t(tmp[j,] - mu_k[,i])
+                }
+        }
+        
+        sigma <- sigma*(1/(n-K))
+        inv.sigma <- solve(sigma) # error.. not invertible
+        
+        ## How do we use score functions?
+        
 }
 
