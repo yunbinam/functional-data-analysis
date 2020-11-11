31061c0a746f5c1e77a21a07ee0f89292c259750
wenbo <563596410@qq.com>
2020-11-10 20:18:28 -0800

    Update simulation_OptimalScore.R

--- regression test file/Simulation/simulation_OptimalScore.R
+++ regression test file/Simulation/simulation_OptimalScore.R
@@ -8,19 +8,21 @@ simu_case1<-function(n=100,v1,v2,v3){
 
   
   Sigma<-diag(642)
-  n1<-3*(n/10)
-  n2<-2*n1
     
   class1<-mvrnorm(n = n, v1, Sigma)
   class2<-mvrnorm(n = n, v2, Sigma)
   class3<-mvrnorm(n = n, v3, Sigma)
 
 
-  X<-rbind(class1,class2,class3)
-  label<-c(rep(0,30),rep(1,30),rep(2,30))
-  return(list(X=X,label=label))
+   X<-rbind(class1,class2,class3)
+  label<-c(rep(0,n),rep(1,n),rep(2,n))
+  indic1<-Reduce(rbind,lapply(1:n,function(x)matrix(c(1,0,0),ncol=3)))
+  indic2<-Reduce(rbind,lapply(1:n,function(x)matrix(c(0,1,0),ncol=3)))
+  indic3<-Reduce(rbind,lapply(1:n,function(x)matrix(c(0,0,1),ncol=3)))
+  indic<-rbind(indic1,indic2,indic3)
+  return(list(X=X,label=label,indic=indic))
 }
 
-data<-simu_case1(n=100,v1,v2,v3)
+data<-simu_case1(n=100,rep(1,642),rep(1,642),rep(1,642))
 
 
